25b127bf378a012b3e2a1475e5a0d17cd2f75bce
lk <1776282511@qq.com>
2019-01-04 17:42:29 +0800

    更新

---
+++ src/assets/js/scroll.js
@@ -0,0 +1,57 @@
+export default {
+	props: {
+		scroller: {
+			type: [ HTMLDocument, Element, Window ],
+			default: window
+		}
+	},
+	mounted() {
+		this.bindScroll()
+	},
+	methods: {
+		bindScroll() {
+			if ( !this.scroller ){
+				return false;
+			}
+			this.handleScroll = ( e ) => {
+				if ( this.onScroll ) this.onScroll() //滑动屏幕回调函数
+			}
+			this.scroller.addEventListener('scroll', this.throttle(this.handleScroll,500,1000));
+		},
+		unbindScroll( scroller ) {
+			scroller = scroller || this.scroller;
+			if ( this.handleScroll ) scroller.removeEventListener( 'scroll',this.throttle(this.handleScroll,500,1000));
+		},
+		//滚动节流
+	 	throttle(func, wait, mustRun) {
+		    let timeout,
+		        startTime = new Date();
+		    return function() {
+		        var context = this,
+		            args = arguments,
+		            curTime = new Date();
+		        clearTimeout(timeout);
+		        // 如果达到了规定的触发时间间隔，触发 handler
+		        if(curTime - startTime >= mustRun){
+		            func.apply(context,args);
+		            startTime = curTime;
+		        // 没达到触发间隔，重新设定定时器
+		        }else{
+		            timeout = setTimeout(func, wait);
+		        }
+		    }
+		}
+	},
+	beforeDestroy() {
+		this.unbindScroll()
+	},
+	watch: {
+		scroller( scroller, oldScroller ) {
+			if ( scroller === oldScroller ){
+				return false;
+			}
+			this.unbindScroll( oldScroller )
+			this.bindScroll( scroller )
+		}
+	}
+}
---
+++ src/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+	<header class="header">
+		<i v-if="$route.path == '/'" class="iconfont icon-ic_menu" @click="toggle"></i>
+		<i v-else class="iconfont icon-ic_back" @click="$router.go(-1)"></i>
+	</header>
+</template>
+<script>
+	export default{
+		methods:{
+			toggle() {
+	            this.$emit('show');
+	        }
+		}
+	}
+</script>
+<style lang="less" scoped>
+	.header {
+		width: 100%;
+		height: 1.5rem;
+		z-index: 9;
+		padding-left: 5%;
+		position: fixed;
+		background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.51) 95%);
+		.iconfont {
+			color: #fff;
+			font-size: 0.8rem;
+			top: 20%;
+			position: relative;
+		}
+	}
+</style>
